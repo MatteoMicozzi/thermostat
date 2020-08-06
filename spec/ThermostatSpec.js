ba3dba776245932fe3e2bf43eb8fb5b3324afeac
Student <student@Makerss-MacBook-Air.local>
2020-08-06 17:20:02 +0100

    refactored tests to create new thermostat before each test

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,24 +1,24 @@
 'use strict';
 describe('Thermostat', function() {
+  var termos;
+  beforeEach(function() {
+    termos = new Thermostat;
+  })
   it('start at 20 degree', function() {
-    let termos = new Thermostat;
     expect(termos.temperature).toEqual(20);
   });
   describe('#up', function() {
     it('raises the temperature', function() {
-      let termos = new Thermostat;
       termos.up();
       expect(termos.temperature).toEqual(21);
     });
   });
   describe('#down', function() {
     it('lower the temperature', function() {
-      let termos = new Thermostat;
       termos.down();
       expect(termos.temperature).toEqual(19);
     });
     it('dont go lower then 10', function() {
-      let termos = new Thermostat;
       termos.temperature = 10;
       termos.down();
       expect(termos.temperature).toEqual(10);
